34fe5b288e729a1ee5121ff329d843f40887d44a
Rudi Luzar <rudi@rudicode.com>
2014-10-23 14:29:03 -0400

    added specs for left,right and forward

--- rover.rb
+++ rover.rb
@@ -1,10 +1,10 @@
 #
-class RoverController
-end
-
 class Rover
   attr_reader :x, :y, :direction
+  attr_accessor :direction
+
   def initialize x_coordinate, y_coordinate, direction
+    # consider if there should be a Point class to inherit from
     @x = x_coordinate
     @y = y_coordinate
     @direction_sequence = ['N','E','S','W']
@@ -25,5 +25,26 @@ class Rover
     @direction = @direction_sequence[index]
   end
 
-
+  def forward
+    case @direction
+    when 'N'
+      @y += 1
+    when 'E'
+      @x += 1
+    when 'W'
+      @x -= 1
+    when 'S'
+      @y -= 1
+    end
+  end
 end
+
+class MissionControl
+  def initialize
+  end
+
+  def execute_control_code code
+    @code = code
+  end
+end
+
--- rover_spec.rb
+++ rover_spec.rb
@@ -1,16 +1,24 @@
 require_relative './rover'
 
-describe RoverController do
+describe MissionControl do
   before :each do
-    test_input = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
+    @test_input = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"
+    @mission_control = MissionControl.new
   end
 
   it "should output correct position of rovers for given input." do
     pending
-    rover_control = RoverController.new
-    rover_control.send_control_code test_input
+    rover_control = MissionControl.new
+    rover_control.execute_control_code @test_input
     rover_control.location.should be "1 3 N\n5 1 E\n"
   end
+
+  it "should create two rovers" do
+    @mission_control.execute_control_code @test_input
+    @rovers = @mission_control.rovers
+    expect(@rovers.count).to eq 2
+  end
+
 end
 
 describe Rover do
@@ -24,25 +32,63 @@ describe Rover do
     @rover.x.should be 1
     @rover.y.should be 2
     @rover.direction.should eq 'N'
+    expect(@rover.direction).to eq 'N'
   end
+
   it "should rotate 90deg left given command left" do
     @rover.left
-    @rover.direction.should eq 'W'
+    expect(@rover.direction).to eq 'W'
     @rover.left
-    @rover.direction.should eq 'S'
+    expect(@rover.direction).to eq 'S'
     @rover.left
-    @rover.direction.should eq 'E'
+    expect(@rover.direction).to eq 'E'
     @rover.left
-    @rover.direction.should eq 'N'
+    expect(@rover.direction).to eq 'N'
   end
+
   it "should rotate 90deg right given command right" do
     @rover.right
-    @rover.direction.should eq 'E'
+    expect(@rover.direction).to eq 'E'
     @rover.right
-    @rover.direction.should eq 'S'
+    expect(@rover.direction).to eq 'S'
     @rover.right
-    @rover.direction.should eq 'W'
+    expect(@rover.direction).to eq 'W'
     @rover.right
-    @rover.direction.should eq 'N'
+    expect(@rover.direction).to eq 'N'
+  end
+
+  describe 'movement' do
+    before :each do
+      #
+    end
+
+    it "should move forward when direction is N" do
+      @rover.direction = 'N'
+      @rover.forward
+      expect(@rover.x).to eq 1
+      expect(@rover.y).to eq 3
+    end
+
+    it "should move forward when direction is E" do
+      @rover.direction = 'E'
+      @rover.forward
+      expect(@rover.x).to eq 2
+      expect(@rover.y).to eq 2
+    end
+
+    it "should move forward when direction is S" do
+      @rover.direction = 'S'
+      @rover.forward
+      expect(@rover.x).to eq 1
+      expect(@rover.y).to eq 1
+    end
+
+    it "should move forward when direction is W" do
+      @rover.direction = 'W'
+      @rover.forward
+      expect(@rover.x).to eq 0
+      expect(@rover.y).to eq 2
+    end
+
   end
 end
