0636e3cefee96a5e0178d289676dda3875f8b72c
Rudi Luzar <rudi@rudicode.com>
2014-10-23 17:13:45 -0400

    Refactored left and right using modulus

--- rover_lib.rb
+++ rover_lib.rb
@@ -16,16 +16,12 @@ class Rover
 
   def left
     index = @direction_sequence.index @direction
-    index = @direction_sequence.count if index == 0
-    index -= 1
-    @direction = @direction_sequence[index]
+    @direction = @direction_sequence[(index-1)%4]
   end
 
   def right
     index = @direction_sequence.index @direction
-    index += 1
-    index = 0 if index >= @direction_sequence.count
-    @direction = @direction_sequence[index]
+    @direction = @direction_sequence[(index+1)%4]
   end
 
   def forward
